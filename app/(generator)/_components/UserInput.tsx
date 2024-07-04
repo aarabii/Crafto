f551d2d0af564d7521a40e5ff520ede47873ff1b
losier <arv95464@gmail.com>
2024-07-04 23:23:49 +0530

    added slider

--- app/(generator)/_components/UserInput.tsx
+++ app/(generator)/_components/UserInput.tsx
@@ -19,6 +19,11 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 
@@ -27,6 +32,8 @@ import MistralIcon from "@/components/Icons/Mistral";
 import GoogleIcon from "@/components/Icons/Google";
 
 import { SelectItemModelContent } from "./SelectItemModelContent";
+import { Slider } from "@/components/ui/slider";
+import { Info } from "lucide-react";
 
 const formSchema = z.object({
   model: z.string().min(1, "Model is required"),
@@ -100,11 +107,11 @@ export const UserInput = () => {
           <fieldset className="grid gap-6 rounded-lg border p-4 bg-background/10 backdrop-blur-sm">
             <legend className="text-lg font-semibold">Settings</legend>
             <div className="grid gap-3">
+              {/* Model */}
               <FormField
                 control={form.control}
                 name="model"
                 render={({ field }) => (
-                  // Model
                   <FormItem>
                     <FormLabel>Model</FormLabel>
                     <Select
@@ -154,6 +161,48 @@ export const UserInput = () => {
                         </SelectItem>
                       </SelectContent>
                     </Select>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              {/* Temp */}
+              <FormField
+                control={form.control}
+                name="temp"
+                render={({ field: { value, onChange } }) => (
+                  <FormItem>
+                    <FormLabel className="flex items-center justify-between pb-2">
+                      <span className="flex items-center justify-center">
+                        Creativity
+                        <Tooltip>
+                          <TooltipTrigger>
+                            <span className="text-accent">
+                              <Info className="w-4 h-4 cursor-pointer" />
+                            </span>
+                          </TooltipTrigger>
+                          <TooltipContent
+                            sideOffset={25}
+                            collisionPadding={20}
+                            className="max-w-sm p-4 rounded-lg shadow-sm"
+                          >
+                            <p className="text-sm text-accent">
+                              The temperature of the model. Higher values
+                              generate more creative text, but may also be less
+                              coherent.
+                            </p>
+                          </TooltipContent>
+                        </Tooltip>
+                      </span>
+                      <span>{value}</span>
+                    </FormLabel>
+                    <Slider
+                      defaultValue={[1]}
+                      min={1}
+                      max={2}
+                      step={0.1}
+                      onValueChange={(val) => onChange(val[0])}
+                    />
                     <FormMessage />
                   </FormItem>
                 )}
--- app/(generator)/layout.tsx
+++ app/(generator)/layout.tsx
@@ -1,3 +1,5 @@
+import { TooltipProvider } from "@/components/ui/tooltip";
+
 export default function generatorLayout({
   children,
 }: {
@@ -9,8 +11,7 @@ export default function generatorLayout({
         <div className="absolute top-0 -z-10 h-full w-full bg-white">
           <div className="absolute bottom-auto left-auto right-0 top-0 h-[500px] w-[500px] -translate-x-[30%] translate-y-[20%] rounded-full bg-[rgba(173,109,244,0.5)] opacity-50 blur-[80px]"></div>
         </div>
-
-        {children}
+        <TooltipProvider>{children}</TooltipProvider>
       </body>
     </html>
   );
