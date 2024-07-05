b76678813a31c8883c37e4791960c4a932a69593
losier <arv95464@gmail.com>
2024-07-05 20:18:24 +0530

    added meta tags and logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,9 +6,49 @@ import { GeistSans } from "geist/font/sans";
 import { cn } from "@/lib/utils";
 
 export const metadata: Metadata = {
-  title: "Crafto",
+  title: "CraftoGram",
   description:
     "Crafto-Gram helps you create unique and engaging Instagram bios and descriptions with ease.",
+  authors: [
+    {
+      name: "Aarab Nishchal",
+      url: "https://aarab.vercel.app",
+    },
+  ],
+  creator: "Aarab Nishchal",
+  referrer: "origin-when-cross-origin",
+  keywords: [
+    "Instagram",
+    "Bio",
+    "Caption",
+    "Generator",
+    "AI",
+    "OpenAI",
+    "GPT-3",
+    "Crafto",
+    "Crafto-Gram",
+    "CraftoGram",
+    "Craftogram",
+    "Crafto-Gram",
+  ],
+  openGraph: {
+    title: "CraftoGram",
+    description:
+      "Crafto-Gram helps you create unique and engaging Instagram bios and descriptions with ease.",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "CraftoGram",
+    description:
+      "Crafto-Gram helps you create unique and engaging Instagram bios and descriptions with ease.",
+    creator: "@aarab_ii",
+  },
+  alternates: {
+    canonical: "/",
+  },
+  icons: "/logo.svg",
+  metadataBase: new URL("https://craftogram.vercel.app"),
+  robots: "index, follow",
 };
 
 export default function RootLayout({
